9df309cb37ec5ed720e394b392719bf87694351c
Roshan De Alwise <roshan.pda@gmail.com>
2026-03-27 22:59:26 +0200

    Inventory - Final updates

--- 2504462.js
+++ 2504462.js
@@ -20,6 +20,7 @@ function startScanner() {
     .start({ facingMode: "environment" }, {}, function (text) {
       const place = JSON.parse(text);
       showMarkerAt(place.top, place.left);
+      showInventory(place.name, place.price, place.inStock); //adding inventory details to json
       toggleScanner();
     })
     .catch(function (err) {
@@ -34,4 +35,19 @@ function stopScanner() {
 function showMarkerAt(top, left) {
   marker.style.top = top;
   marker.style.left = left;
+}
+
+// Function to display inventory details
+function showInventory(name, price, inStock) {
+  // Get references to HTML elements
+  const inventory = document.getElementById("inventory");
+  const itemName = document.getElementById("itemName");
+  const itemStock = document.getElementById("itemStock");
+  const itemPrice = document.getElementById("itemPrice");
+
+  inventory.style.display = "block"; // Make the inventory section visible
+
+  itemName.textContent = "Item Name: " + name; 
+  itemStock.textContent = "Available in Store: " + (inStock ? "Yes" : "No");
+  itemPrice.textContent = "Item Price: €" + price.toFixed(2);
 }
